d3b9702db7db5232c20a80764bacee9b36a5a7a9
shr968 <shreyanayakb.261104@gmail.com>
2025-01-10 23:36:15 +0530

    changes in bcrypt

--- login-signup/login.js
+++ login-signup/login.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require("body-parser");
 const session=require('express-session')
-const bcrypt = require('bcrypt');  
+const bcrypt = require('bcryptjs');  
 const encoder = bodyParser.urlencoded();
 const app = express();
 app.use("/assets", express.static("assets"));
