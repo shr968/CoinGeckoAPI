d2c6ffc50306a113ab9f25b348e178f1c476592f
shr968 <shreyanayakb.261104@gmail.com>
2025-01-11 00:00:45 +0530

    hosting problem1

--- login-signup/login.js
+++ login-signup/login.js
@@ -20,6 +20,7 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: 3306
 });
 
 connection.connect(function (error) {
