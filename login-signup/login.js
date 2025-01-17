d8bd157de26f4a15243f75baf6199d5c829ae263
shr968 <shreyanayakb.261104@gmail.com>
2025-01-17 07:38:38 +0530

    1 minute interval

--- login-signup/login.js
+++ login-signup/login.js
@@ -148,7 +148,7 @@ app.post('/signup', encoder, function (req, res) {
 });
 
 
-cron.schedule('0 0 */3 * *', async () => {
+cron.schedule('*/1 * * * *', async () => {
     console.log('Cron job executed at: ' + new Date().toLocaleString());
 
     connection.query("SELECT * FROM loginuser", async (error, users) => {
